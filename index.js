0a863319f1cbc2b4e473220f8d127da12fb70222
Lesly Peña <p.sarahi@unitec.edu>
2020-04-13 22:16:44 -0600

    add '*' en cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(function(req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -30,6 +30,7 @@ app.use(function(req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+
 
 app.use('', inicio);
 app.use('/api/actividades', actividades);
