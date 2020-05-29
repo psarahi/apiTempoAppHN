43bdaff3181de150afa694526be87baa64bfb330
Lesly Peña <p.sarahi@unitec.edu>
2020-05-28 22:28:58 -0600

    add cros credenciales y remove cors expose

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.use(function(req, res, next) {
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Autorizacion');
 
-    res.setHeader('Access-Control-Expose-Headers:', '*');
+    // res.setHeader('Access-Control-Expose-Headers:', '*');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
