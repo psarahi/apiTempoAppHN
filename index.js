8e5a344d4ae358a6b506262c5921eaec0248143e
Lesly Peña <p.sarahi@unitec.edu>
2020-05-28 22:24:51 -0600

    Cambios cors setHeaders

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ app.use(express.json());
 app.use(function(req, res, next) {
 
     // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Autorizacion');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Autorizacion');
 
-    res.header('Access-Control-Expose-Headers:', '*, Autorizacion');
+    res.setHeader('Access-Control-Expose-Headers:', '*');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
