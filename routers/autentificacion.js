9bf2ca27a3d6d0bd4c6d6cddaa3714670690670d
Lesly Peña <p.sarahi@unitec.edu>
2020-05-31 23:22:27 -0600

    test 3

--- routers/autentificacion.js
+++ routers/autentificacion.js
@@ -30,7 +30,7 @@ router.post('/', async(req, res) => {
 
             const jwtToken = usuarioMiembro.generarJWT();
 
-            res.status(201).header('Authorization', jwtToken).send(jwtToken);
+            res.status(201).header('Authorization', jwtToken).send('kkkkk');
         }
     } else {
         let passwordValidaCuenta = await bcrypt.compare(req.body.password, usuarioCuenta.password);
@@ -38,7 +38,7 @@ router.post('/', async(req, res) => {
 
         const jwtToken = usuarioCuenta.generarJWT();
 
-        res.status(201).header('Authorization', jwtToken).send(jwtToken);
+        res.status(201).header('Authorization', jwtToken).send('kkkkkk');
     }
 
 });
