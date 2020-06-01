d3e043fe4e2e2aadadae5021e8b0115e5686cdd6
Lesly Peña <p.sarahi@unitec.edu>
2020-06-01 00:10:12 -0600

    error log auth

--- routers/autentificacion.js
+++ routers/autentificacion.js
@@ -7,38 +7,44 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 router.post('/', async(req, res) => {
+    try {
+        let usuarioMiembro = await Miembro.findOne({
+            $and: [
+                { usuario: req.body.usuario },
+                { estado: true }
+            ]
+        });
+        let usuarioCuenta = await Cuenta.findOne({
+            $and: [
+                { usuario: req.body.usuario },
+                { estado: true }
+            ]
+        });
 
-    let usuarioMiembro = await Miembro.findOne({
-        $and: [
-            { usuario: req.body.usuario },
-            { estado: true }
-        ]
-    });
-    let usuarioCuenta = await Cuenta.findOne({
-        $and: [
-            { usuario: req.body.usuario },
-            { estado: true }
-        ]
-    });
+        if (!usuarioCuenta) {
+            if (!usuarioMiembro) {
+                return res.status(400).send('Usuario o contraseña incorrectas');
+            } else {
+                let passwordValidaMiembro = await bcrypt.compare(req.body.password, usuarioMiembro.password);
+                if (!passwordValidaMiembro) return res.status(400).send('Usuario o contraseña incorrectas');
 
-    if (!usuarioCuenta) {
-        if (!usuarioMiembro) {
-            return res.status(400).send('Usuario o contraseña incorrectas');
+                const jwtToken = usuarioMiembro.generarJWT();
+
+                res.status(201).header('Authorization', jwtToken).send(jwtToken);
+            }
         } else {
-            let passwordValidaMiembro = await bcrypt.compare(req.body.password, usuarioMiembro.password);
-            if (!passwordValidaMiembro) return res.status(400).send('Usuario o contraseña incorrectas');
+            let passwordValidaCuenta = await bcrypt.compare(req.body.password, usuarioCuenta.password);
+            if (!passwordValidaCuenta) return res.status(400).send('Usuario o contraseña incorrectas');
 
-            const jwtToken = usuarioMiembro.generarJWT();
+            const jwtToken = usuarioCuenta.generarJWT();
 
             res.status(201).header('Authorization', jwtToken).send(jwtToken);
         }
-    } else {
-        let passwordValidaCuenta = await bcrypt.compare(req.body.password, usuarioCuenta.password);
-        if (!passwordValidaCuenta) return res.status(400).send('Usuario o contraseña incorrectas');
 
-        const jwtToken = usuarioCuenta.generarJWT();
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error);
 
-        res.status(201).header('Authorization', jwtToken).send(jwtToken);
     }
 
 });
