c2ed5829a882f604c4bb92141504733ca07eed7b
Lesly Peña <p.sarahi@unitec.edu>
2020-07-16 13:24:12 -0600

    cambio new Date(req.body.inicio).toISOString()

--- routers/detalleActividad.js
+++ routers/detalleActividad.js
@@ -354,9 +354,9 @@ router.post("/", async(req, res) => {
             cuentas: req.body.cuentas,
             programacionequipos: req.body.programacionequipos,
             descripcion: req.body.descripcion,
-            inicio: req.body.inicio.toISOString(),
-            fecha: req.body.fecha.toISOString(),
-            fin: req.body.fin.toISOString(),
+            inicio: new Date(req.body.inicio).toISOString(),
+            fecha: new Date(req.body.fecha).toISOString(),
+            fin: new Date(req.body.fin).toISOString(),
             estado: req.body.estado,
         });
 
@@ -486,9 +486,9 @@ router.put("/:_id", async(req, res) => {
                 cuentas: req.body.cuentas,
                 programacionequipos: req.body.programacionequipos,
                 descripcion: req.body.descripcion,
-                inicio: req.body.inicio.toISOString(),
-                fecha: req.body.fecha.toISOString(),
-                fin: req.body.fin.toISOString(),
+                inicio: new Date(req.body.inicio).toISOString(),
+                fecha: new Date(req.body.fecha).toISOString(),
+                fin: new Date(req.body.fin).toISOString(),
                 estado: req.body.estado,
             }, {
                 new: true,
